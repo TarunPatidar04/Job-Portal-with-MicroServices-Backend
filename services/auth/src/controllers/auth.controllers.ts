50868509292e49e31c34f9510382705adcd0ecf8
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-02-14 11:33:55 +0530

    feat: Implement user registration endpoint with file upload and password hashing.

--- services/auth/src/controllers/auth.controllers.ts
+++ services/auth/src/controllers/auth.controllers.ts
@@ -1,5 +1,41 @@
+import { sql } from "../utils/db.js";
+import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
+import bcrypt from "bcrypt";
 
 export const registerUser = TryCatch(async (req, res, next) => {
-    
+  const { name, email, password, phoneNumber, role, bio } = req.body;
+
+  if (!name || !email || !phoneNumber || !role) {
+    throw new ErrorHandler(400, "Please fill all details");
+  }
+
+  const existingUser =
+    await sql`SELECT user_id FROM users WHERE email=${email}`;
+
+  if (existingUser.length > 0) {
+    throw new ErrorHandler(409, "User with this email already exits");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  let registerUser;
+
+  if (role === "recruiter") {
+    const [user] =
+      await sql`INSERT INTO users (name,email,password,phone_number,role) VALUES
+      (${name} ,${email},${hashPassword},${phoneNumber},${role} )
+      RETURNING user_id ,name,email,phone_number,role,created_at`;
+
+    registerUser = user;
+  } else if (role === "jobseeker") {
+    const file = req.file;
+    const [user] =
+      await sql`INSERT INTO users (name,email,password,phone_number,role) VALUES
+      (${name} ,${email},${hashPassword},${phoneNumber},${role} )
+      RETURNING user_id ,name,email,phone_number,role,created_at`;
+
+    registerUser = user;
+  }
+  res.json(registerUser);
 });
--- services/auth/src/routes/auth.route.ts
+++ services/auth/src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { registerUser } from "../controllers/auth.controllers.js";
+import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
+
+
+router.post("/register",uploadFile,registerUser)
 
 export default router;
