40d45c8063eb1d136325b81dbfee6e6e6792da0a
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-02-16 14:58:28 +0530

    chore: Replace hardcoded SMTP credentials with environment variables.

--- services/utils/src/consumer.ts
+++ services/utils/src/consumer.ts
@@ -33,8 +33,8 @@ export const startSendMailConsumer = async () => {
             port: 465,
             secure: true,
             auth: {
-              user: "tarun.coderfarm@gmail.com",
-              pass: "ocdq eutn jdxo hzzz",
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASS,
             },
           });
 
