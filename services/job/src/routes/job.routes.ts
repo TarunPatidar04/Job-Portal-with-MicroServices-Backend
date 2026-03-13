dc9a8fcd3b5e015ece8aae27c5ec67df0a6a6eba
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-03-13 11:07:20 +0530

    feat: Add job creation endpoint and controller logic.

--- services/job/src/routes/job.routes.ts
+++ services/job/src/routes/job.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createCompany,
+  createJob,
   deleteCompany,
 } from "../controllers/job.controllers.js";
 import { isAuth } from "../middleware/auth.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/company/new", isAuth, uploadFile, createCompany);
 router.delete("/company/:companyId", isAuth, deleteCompany);
+router.post("/new", isAuth, createJob);
 
 export default router;
